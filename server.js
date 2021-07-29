c2ddeccbfbf6f838feba0f82975f65ef813a7670
Ian Fullmer <ianfullmer@yahoo.com>
2021-07-28 20:57:01 -0700

    1st 3 advanced node/express challenges

--- server.js
+++ server.js
@@ -3,16 +3,29 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+var session = require('express-session');
+var passport = require('passport');
 
 const app = express();
 
 fccTesting(app); //For FCC testing purposes
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.set('view engine', 'pug')
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.route('/').get((req, res) => {
-  res.render('Load your view here');
+  res.render(process.cwd() + '/views/pug', {title: 'Hello', message: 'Please login'});
 });
 
 const PORT = process.env.PORT || 3000;
