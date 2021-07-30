e436973e07e00d90de6f55f17f6838db936531d8
Ian Fullmer <ianfullmer@yahoo.com>
2021-07-29 17:23:01 -0700

    Implement the Serialization of a Passport User attempt 1

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # [Introduction to Advanced Node and Express Challenges](https://www.freecodecamp.org/learn/quality-assurance/advanced-node-and-express/)
+# https://serene-brushlands-23443.herokuapp.com/
--- server.js
+++ server.js
@@ -11,17 +11,6 @@ const app = express();
 
 fccTesting(app); //For FCC testing purposes
 
-passport.serializeUser((user, done) => {
-  done(null, user._id);
-});
-
-// passport.deserializeUser((id, done) => {
-//   myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-//     done(null, null);
-//   });
-// });
-
-
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: true,
@@ -36,9 +25,28 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+myDB(async client => {
+  const myDataBase = await client.db('database').collection('users')
+
 app.route('/').get((req, res) => {
-  res.render(process.cwd() + '/views/pug', {title: 'Hello', message: 'Please login'});
+  res.render('pug', {title: 'Connected to Database', message: 'Please login'});
 });
+
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+});
+
+passport.deserializeUser((id, done) => {
+  myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+    done(null, doc);
+  });
+});
+}).catch(e => {
+  app.route('/').get((req, res) => {
+    res.render('pug', { title: e, message: 'Unable to login' });
+  });
+});
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
