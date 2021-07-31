f3a7c41f0a01452587c11fb4e3e545b6a825e221
Ian Fullmer <ianfullmer@yahoo.com>
2021-07-31 14:10:42 -0700

    how to put a profile together take 1

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // ensureAuthenticated as a middleware to the request for the profile page
 app.route('/profile').get(ensureAuthenticated, (req,res) => {
-  res.render(process.cwd() + '/views/pug/profile');
+  res.render(process.cwd() + '/views/pug/profile', {username: req.user.username});
 });
 
 myDB(async client => {
