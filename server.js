511f8f860b4e7e6d4a8d6a4ef860db846e725f80
Ian Fullmer <ianfullmer@yahoo.com>
2021-07-31 16:18:49 -0700

    Registration of New Users take 1

--- server.js
+++ server.js
@@ -35,62 +35,92 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // ensureAuthenticated as a middleware to the request for the profile page
-app.route('/profile').get(ensureAuthenticated, (req,res) => {
-  res.render(process.cwd() + '/views/pug/profile', {username: req.user.username});
+app.route('/profile').get(ensureAuthenticated, (req, res) => {
+  res.render(process.cwd() + '/views/pug/profile', { username: req.user.username });
 });
 
 
 myDB(async client => {
   const myDataBase = await client.db('database').collection('users')
 
-  
-app.route('/').get((req, res) => {
-  res.render('pug', {showLogin: true,title: 'Connected to Database', message: 'Please login'});
-});
 
-app.route('/login').post(passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
-  res.redirect('/profile')
-});
-
-app.route('/logout')
-  .get((req, res) => {
-    req.logout();
-    res.redirect('/');
-});
-
-app.use((req, res, next) => {
-  res.status(404)
-    .type('text')
-    .send('Not Found');
-});
-
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    myDataBase.findOne({ username: username }, function (err, user) {
-      console.log('User '+ username +' attempted to log in.');
-      if (err) { return done(err); }
-      if (!user) { return done(null, false); }
-      if (password !== user.password) { return done(null, false); }
-      return done(null, user);
-    });
-  }
-));
-
-passport.serializeUser((user, done) => {
-  done(null, user._id);
-});
-
-passport.deserializeUser((id, done) => {
-  myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-    done(null, doc);
+  app.route('/').get((req, res) => {
+    res.render('pug', { showLogin: true, title: 'Connected to Database', message: 'Please login', showRegistration: true });
   });
-});
+
+  app.route('/register')
+    .post((req, res, next) => {
+      myDataBase.findOne({ username: req.body.username }, function (err, user) {
+        if (err) {
+          next(err);
+        } else if (user) {
+          res.redirect('/');
+        } else {
+          myDataBase.insertOne({
+            username: req.body.username,
+            password: req.body.password
+          },
+            (err, doc) => {
+              if (err) {
+                res.redirect('/');
+              } else {
+                // The inserted document is held within
+                // the ops property of the doc
+                next(null, doc.ops[0]);
+              }
+            }
+          )
+        }
+      })
+    },
+      passport.authenticate('local', { failureRedirect: '/' }),
+      (req, res, next) => {
+        res.redirect('/profile');
+      }
+    );
+  app.route('/login').post(passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
+    res.redirect('/profile')
+  });
+
+  app.route('/logout')
+    .get((req, res) => {
+      req.logout();
+      res.redirect('/');
+    });
+
+  app.use((req, res, next) => {
+    res.status(404)
+      .type('text')
+      .send('Not Found');
+  });
+
+  passport.use(new LocalStrategy(
+    function (username, password, done) {
+      myDataBase.findOne({ username: username }, function (err, user) {
+        console.log('User ' + username + ' attempted to log in.');
+        if (err) { return done(err); }
+        if (!user) { return done(null, false); }
+        if (password !== user.password) { return done(null, false); }
+        return done(null, user);
+      });
+    }
+  ));
+
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+      done(null, doc);
+    });
+  });
 }).catch(e => {
   app.route('/').get((req, res) => {
     res.render('pug', { title: e, message: 'Unable to login' });
   });
-  
-  
+
+
 });
 
 
